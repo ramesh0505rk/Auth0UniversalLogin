c54ccf4ce4f05fc3a94b907c139632908e7eff62
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-04-03 23:25:52 +0530

    Implemented Callback, Login, Home components. Updated app config and app routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,19 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAuth0({
+      domain: 'dev-7clsee1nzr8ucaon.us.auth0.com',
+      clientId: 'kEiYOOyLnSw1dGOy59GgHyR3sr0K9Qet',
+      authorizationParams: {
+        redirect_uri: 'http://localhost:4200/callback'
+      }
+    })
+  ]
 };
